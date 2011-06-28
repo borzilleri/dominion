f6f9579a667eaa5fdc5c90cf3d636eea2cc81e79
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-06-27 21:26:29 -0700

    Add QR code to non-mobile browsers, hopefully

--- public/javascript/bootstrap.js
+++ public/javascript/bootstrap.js
@@ -1,6 +1,14 @@
 $(function() {
+  window.isMobile = (function(ua) {
+    return (
+      ua.match('/iPhone/') ||
+      ua.match('/iPod/') ||
+      ua.match('/iPad/') ||
+      ua.match('/Android/')
+    );
+  })(navigator.userAgent);
+
   window.options = new Config_Model();
   window.app = new Dominion_Controller();
   Backbone.history.start();
-
 });
